06bb682ff2ee4bde1590cf56202e0fc3354f83df
ansa1 <abirovansat22@gmail.com>
2018-10-03 20:31:35 +0300

    new logo

--- src/containers/MainMenu.js
+++ src/containers/MainMenu.js
@@ -3,13 +3,16 @@ import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import 'assets/styles/MainMenu.css'
-import TopPanel from "components/InfoPanel";
+import logo2 from 'assets/img/logo2.svg'
 
 class MainMenu extends Component {
     render() {
         return (
             <div className='main-menu-container'>
-                <TopPanel/>
+                <div className='main-menu-container-text'>
+                    Let`s build the best interface ever!
+                </div>
+                <img src={logo2}/>
             </div>
         )
     }
