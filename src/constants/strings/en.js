5764f039b72c40963c47111651a0695ec8f0be87
petrNikolaev1 <petru4o144@gmail.com>
2018-10-03 22:13:37 +0300

    en dictionary logic

---
+++ src/constants/strings/en.js
@@ -0,0 +1,25 @@
+export default {
+
+    OrderList: {
+        ID: 'id',
+        FROM: 'Departure address',
+        TO: 'Delivery address',
+        STATUS: 'Status',
+        BIRTH_DATE: 'Birth date'
+    },
+
+    OrderItem: {
+        ID: 'id',
+        FROM: 'Departure address',
+        TO: 'Delivery address',
+        STATUS: 'Status',
+        FUll_FROM: 'Full departure address',
+        WEIGHT: 'Weight',
+        WORTH: 'Worth',
+        DESCRIPTION: 'Description',
+        PHONE_NUM: 'Phone number',
+        DUE_DATE: 'Delivery date',
+        BIRTH_DATE: 'Birth date'
+    }
+
+};
