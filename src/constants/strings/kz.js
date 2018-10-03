da52c5f045ed669fb3cf240784c2c80cff5c7135
ansa1 <abirovansat22@gmail.com>
2018-10-03 22:39:25 +0300

    new language

---
+++ src/constants/strings/kz.js
@@ -0,0 +1,26 @@
+export default {
+
+    OrderList: {
+        ID: 'id',
+        FROM: 'Ұшу нүктесі',
+        TO: 'Жеткізу орны',
+        STATUS: 'Күйі',
+        BIRTH_DATE: 'Өтініш күні'
+    },
+
+    OrderItem: {
+        ID: 'id',
+        FROM: 'Ұшу нүктесі',
+        TO: 'Жеткізу орны',
+        STATUS: 'Күйі',
+        FULL_TO: 'Test',
+        FUll_FROM: 'Жеткізу мекен-жайы',
+        WEIGHT: 'Салмақ',
+        WORTH: 'Құны',
+        DESCRIPTION: 'Сипаттама',
+        PHONE_NUM: 'Телефон нөмірі',
+        DUE_DATE: 'Жеткізу',
+        BIRTH_DATE: 'Қолданба күні'
+    }
+
+};
