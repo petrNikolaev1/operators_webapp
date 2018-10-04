3c61bf48b05a5913b757705b67ce1b86fb6fa348
shishi7 <rus.riko@gmail.com>
2018-10-04 16:55:54 +0300

    LOADING REFACTORED

--- src/common/Loading.js
+++ src/common/Loading.js
@@ -4,18 +4,16 @@ Component representing the menu of a device adding.
 
 import React, {Component} from 'react';
 
-import '../assets/styles/Loading.scss'
-import logo2 from '../assets/img/logo2white.svg'
-import showBeforeHoc from '../hocs/showBeforeHOC'
+import '@/assets/styles/Loading.scss'
+import logo from '@/assets/img/logo2white.svg'
+import showBeforeHoc from '@/hocs/showBeforeHOC'
 
 class Loading extends Component {
 
     render() {
         return (
             <div className="loading-container">
-                <div className='loading-item'>
-                    <img className='second' src={logo2} alt='logo2'/>
-                </div>
+                    <img className='second' src={logo} alt='logo'/>
             </div>
         )
     }
