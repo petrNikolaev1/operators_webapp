61fac46aaeccbc31a4375ae11e1155729fb5c45b
Nikolay Matyashov <lave0chance@gmail.com>
2018-10-24 19:37:47 +0300

    Setting page update
    Logout button moved from top to bottom and become red
    Expanding blocks of administration panel and personal info added

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,91 +1,129 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import {Reply, ExitToApp} from '@material-ui/icons';
+import {Reply, ExitToApp, ExpandMore} from '@material-ui/icons';
 import Select from '@/common/Select'
 import OperatorPhoto from '@/assets/img/personal_photo.jpg'
 
 import '@/assets/styles/Settings.scss'
+import classNames from "classnames";
 
 export default class Settings extends Component {
+    state = {
+        administrationPanelShown: false,
+        personalInfoShown: false,
+    };
+
+    showPersonalInfo = () => {
+        this.setState(prevState => ({personalInfoShown: !prevState.personalInfoShown}))
+    };
+
+    showAdministrationPanel = () => {
+        this.setState(prevState => ({administrationPanelShown: !prevState.administrationPanelShown}))
+    };
+
     render() {
+        const {administrationPanelShown, personalInfoShown} = this.state;
+
+        const administrationPanelOpened = administrationPanelShown;
+        const personalInfoOpened = personalInfoShown;
+
         return (
-            <div className="settings">
-                <div className='settings-title'>
-                    <div className='settings-title-label'>
-                        Settings
+            <div>
+                <div className="settings">
+                    <div className='settings-title'>
+                        <div className='settings-title-label'>
+                            Settings
+                        </div>
+                        <Link to="/" className='settings-back-to-menu'>
+                            <Reply/>
+                            Back to main menu
+                        </Link>
                     </div>
-                    <div className="settings-title-buttons">
-                        <div className="settings-title-buttons-link">
-                            <Link to="/" className='settings-back-to-menu'>
-                                Back to main menu
-                                <Reply/>
-                            </Link>
+                    <div className='settings-info'>
+                        <div className="settings-info-title">
+                            <div className="settings-info-title-text">
+                                Administration panel
+                            </div>
+                            <div className="settings-info-title-expand">
+                                <ExpandMore
+                                    className={classNames('home-block-title-expand', {'reversed': administrationPanelOpened})}
+                                    onClick={this.showAdministrationPanel}
+                                />
+                            </div>
                         </div>
-                        <div className="settings-title-buttons-link">
-                            <Link to="/login" className='settings-logout'>
-                                Logout
-                                <ExitToApp/>
-                            </Link>
+                        {administrationPanelOpened &&
+                        <div className='settings-info-drivers'>
+                            <div className='settings-info-drivers-label'>
+                                Select number of drivers
+                            </div>
+                            <div className='settings-info-drivers-select'>
+                                <Select
+                                    isSerchable={true}
+                                    noOptionsMessage={'No Options'}
+                                    placeholder={'3'}
+                                    //TODO options={}
+                                    formClassName='default-select'
+                                />
+                            </div>
+                        </div>}
+                        <div className="settings-info-title">
+                            <div className="settings-info-title-text">
+                                Personal information
+                            </div>
+                            <div className="settings-info-title-expand">
+                                <ExpandMore
+                                    className={classNames('home-block-title-expand', {'reversed': personalInfoOpened})}
+                                    onClick={this.showPersonalInfo}
+                                />
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className='settings-info'>
-                    <div className='settings-info-drivers'>
-                        <div className='settings-info-drivers-label'>
-                            Select number of drivers
-                        </div>
-                        <div className='settings-info-drivers-select'>
-                            <Select
-                                isSerchable={true}
-                                noOptionsMessage={'No Options'}
-                                placeholder={'3'}
-                                //TODO options={}
-                                formClassName='default-select'
-                            />
-                        </div>
-                    </div>
-                    <div className="settings-info-title">
-                        Personal information
-                    </div>
-                    <div className='settings-info-operator'>
-                        <div className="settings-info-operator-photo">
-                            <img src={OperatorPhoto} alt="OperatorPhoto"/>
-                        </div>
-                        <div className="settings-info-operator-personal-info">
-                            <div className="settings-info-operator-personal-info-entry">
+                        {personalInfoOpened &&
+                        <div className='settings-info-operator'>
+                            <div className="settings-info-operator-photo">
+                                <img src={OperatorPhoto} alt="OperatorPhoto"/>
+                            </div>
+                            <div className="settings-info-operator-personal-info">
+                                <div className="settings-info-operator-personal-info-entry">
                                 <span className="settings-info-operator-personal-info-entry-name">
                                     Full Name:
                                 </span>
-                                <span className="settings-info-operator-personal-info-entry-value">
+                                    <span className="settings-info-operator-personal-info-entry-value">
                                     Matyashov Nikolay Alekseevich
                                 </span>
-                            </div>
-                            <div className="settings-info-operator-personal-info-entry">
+                                </div>
+                                <div className="settings-info-operator-personal-info-entry">
                                 <span className="settings-info-operator-personal-info-entry-name">
                                     Licence Number:
                                 </span>
-                                <span className="settings-info-operator-personal-info-entry-value">
+                                    <span className="settings-info-operator-personal-info-entry-value">
                                     A982FBV0
                                 </span>
-                            </div>
-                            <div className="settings-info-operator-personal-info-entry">
+                                </div>
+                                <div className="settings-info-operator-personal-info-entry">
                                 <span className="settings-info-operator-personal-info-entry-name">
                                     Position:
                                 </span>
-                                <span className="settings-info-operator-personal-info-entry-value">
+                                    <span className="settings-info-operator-personal-info-entry-value">
                                     Junior operator
                                 </span>
-                            </div>
-                            <div className="settings-info-operator-personal-info-entry">
+                                </div>
+                                <div className="settings-info-operator-personal-info-entry">
                                 <span className="settings-info-operator-personal-info-entry-name">
                                     Access Level:
                                 </span>
-                                <span className="settings-info-operator-personal-info-entry-value">
+                                    <span className="settings-info-operator-personal-info-entry-value">
                                     Yellow
                                 </span>
+                                </div>
                             </div>
-                        </div>
+                        </div>}
                     </div>
+                </div>
+                <div className="btn">
+                    <Link to="/login" className='btn-logout'>
+                        Logout
+                        <ExitToApp/>
+                    </Link>
                 </div>
             </div>
         )
