e752865d7df441f8c628b3892984a912feebc59b
USER_2 <petru4o144@gmail.com>
2018-11-08 02:02:27 +0300

    api address fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const DRIVER_REFRESH_RATE = 30; // Refresh rate in ms
 
 export const GOOGLE_API_KEY = 'AIzaSyAbChC4mhcoyeibPK_o8rNHjjgVffObCdw';
 
-export const SERVER_URL = 'https://innodelivery.online:8443/v1/operator/';
+export const SERVER_URL = 'https://innodelivery.online:443/v1/operator/';
 
 export default keyMirror({
     CHANGE_LANG: null,
